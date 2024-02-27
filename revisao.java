c2c517d525bdafe336557bd1b2b8f1aba108d9cb
pedrohmaiaoliv <pedrohmaiaoliv@gmail.com>
2024-02-27 20:48:33 -0300

    Com verificação do while de variável de repetição

--- revisao.java
+++ revisao.java
@@ -8,32 +8,48 @@ public class revisao {
         System.out.println("Bem vindo ao programa para descobrir se o aluno vai passar!");
         System.out.println("Primeiramente, preciso saber, quantos alunos possui na sala? ");
         Scanner sc = new Scanner(System.in);
+
+        double nota1;
+        double nota2;
+        double media;
         int num_alunos = sc.nextInt();
-        for (int n = 0; n< num_alunos; n++) {
+
+            for (int n = 0; n< num_alunos; n++) {
 
 
-        
-            System.out.print ("digite a primeira nota: ");
-            double nota1 = sc.nextDouble();
+                System.out.println("Agora vamos digitar as notas?");
+                
+                    do { System.out.print ("digite a primeira nota: ");
+                        nota1 = sc.nextDouble();
+                        if (nota1 < 0 || nota1 > 10) {
+                        System.out.println("Não existe nota menor que 0 ou maior que 10!");
+                        }
+                    }while (nota1 < 0 || nota1 > 10);
 
-            System.out.print ("digite a segunda nota: ");
-            double nota2 = sc.nextDouble();
+                
+                do{ System.out.print ("digite a segunda nota: ");
+                nota2 = sc.nextDouble();
+                        if (nota2 < 0 || nota2 > 10) {
+                        System.out.println("Não existe nota menor que 0 ou maior que 10!!");
+                        }
+                    }while (nota2 < 0 || nota2 > 10);
+                
 
-            double media = (nota1+nota2)/2;
+                media = (nota1+nota2)/2;
 
-            System.out.println("A média do aluno é: " + media);
+                System.out.println("A média do aluno é: " + media);
 
-                if(media >=9){
-                    System.out.println("Parabéns, Excelente!");
+                    if(media >=9){
+                        System.out.println("Parabéns, Excelente!");
 
-                }else if(media >= 6 ) {
-                    System.out.println("Aluno aprovado, ok né?");
-                    
-                }else if(media >= 4) {
-                    System.out.println("Aluno em recuepração! vai fazer N3 em!");
-                }else{
-                    System.out.println("Ihhhhhhhhhhh!!");
+                    }else if(media >= 6 ) {
+                        System.out.println("Aluno aprovado, ok né?");
+                        
+                    }else if(media >= 4) {
+                        System.out.println("Aluno em recuepração! vai fazer N3 em!");
+                    }else{
+                        System.out.println("Ihhhhhhhhhhh!!");
             }
+        }
     }
-}
 }
