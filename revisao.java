70fc666a5a515cd375c84831906709cb1c450363
pedrohmaiaoliv <pedrohmaiaoliv@gmail.com>
2024-02-27 20:28:21 -0300

    yes

--- revisao.java
+++ revisao.java
@@ -6,28 +6,34 @@ public class revisao {
         
 
         System.out.println("Bem vindo ao programa para descobrir se o aluno vai passar!");
-
+        System.out.println("Primeiramente, preciso saber, quantos alunos possui na sala? ");
         Scanner sc = new Scanner(System.in);
-        System.out.print ("digite a primeira nota: ");
-        double nota1 = sc.nextDouble();
+        int num_alunos = sc.nextInt();
+        for (int n = 0; n< num_alunos; n++) {
 
-        System.out.print ("digite a segunda nota: ");
-        double nota2 = sc.nextDouble();
 
-        double media = (nota1+nota2)/2;
+        
+            System.out.print ("digite a primeira nota: ");
+            double nota1 = sc.nextDouble();
 
-        System.out.println("A média do aluno é: " + media);
+            System.out.print ("digite a segunda nota: ");
+            double nota2 = sc.nextDouble();
 
-            if(media >=9){
-                System.out.println("Parabéns, Excelente!");
+            double media = (nota1+nota2)/2;
 
-            }else if(media >= 6 ) {
-                System.out.println("Aluno aprovado, ok né?");
-                
-            }else if(media >= 4) {
-                System.out.println("Aluno em recuepração! vai fazer N3 em!");
-            }else{
-                System.out.println("Ihhhhhhhhhhh!!");
+            System.out.println("A média do aluno é: " + media);
+
+                if(media >=9){
+                    System.out.println("Parabéns, Excelente!");
+
+                }else if(media >= 6 ) {
+                    System.out.println("Aluno aprovado, ok né?");
+                    
+                }else if(media >= 4) {
+                    System.out.println("Aluno em recuepração! vai fazer N3 em!");
+                }else{
+                    System.out.println("Ihhhhhhhhhhh!!");
             }
     }
+}
 }
